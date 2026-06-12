b205eb2bbfb0c8c4b0e90409743a78b3ef1cfa9a
kastashenkova <astashenkova.katya@gmail.com>
2026-06-12 11:17:04 +0300

    fix version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:6.1.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.1.0")
 
-    testImplementation("org.junit.platform:junit-platform-suite-engine:1.10.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:6.0.2")
+    testImplementation("org.junit.platform:junit-platform-suite-engine:6.1.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:6.1.0")
 }
 
 tasks.test {
